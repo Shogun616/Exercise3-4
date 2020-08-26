7f8fe01e2cf9620b0395b14630822a98016936b4
Philip Mattsson <maini.ac@hotmail.com>
2020-08-26 15:20:52 +0200

    Version 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,21 +6,37 @@ public class Main {
 
     static Scanner scan = new Scanner(System.in);
 
-    static String word = "";
+    static String word;
 
-    static int number = (int)(1+Math.random()*10);
+    static int number;
 
     public static void main(String[] args) {
 
-        System.out.print("Skriv ett text: ");
+        boolean repeat = true;
+
+        while (repeat) {
+
+            input();
+
+            if (number <= 10) {
+                for (int i = 0; i < number; i++) {
+                    System.out.print(word + " ");
+
+                }
+
+            } else
+                repeat = false;
+        }
+        System.out.println("Du har överstig antal tal!");
+    }
+
+    static void input() {
+
+        System.out.print("\nSkriv ett text: ");
         word = scan.nextLine();
 
         System.out.print("Skriv ett tal: ");
         number = scan.nextInt();
-
-        while (number <= 10){
-            System.out.println(word);
-            number++;
-        }
+        scan.nextLine();
     }
 }
